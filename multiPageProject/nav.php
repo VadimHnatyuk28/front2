8f967dff9af0b2f72c098e0a5c6f8e8f59cfd5a0
VadimHnatyuk28 <vadim.gnatyuk28@gmail.com>
2022-11-21 14:30:10 +0200

    chanced file`s

---
+++ multiPageProject/nav.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#" >Головне меню</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Головна сторінка</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="#">Галерея</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Контакти</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
