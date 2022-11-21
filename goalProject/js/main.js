87c14e549f13e476723562a41550563c83112a12
VadimHnatyuk28 <vadim.gnatyuk28@gmail.com>
2022-11-21 15:58:40 +0200

    new project

---
+++ goalProject/js/main.js
@@ -0,0 +1,29 @@
+var btn=document.getElementById('btn');
+btn.onclick=function(e){
+    e.preventDefault();
+   document.querySelector('.img').style.display='none';
+}
+
+
+$(function () {
+	$(window).scroll(function() {
+	    $('#learn .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('h2').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDown");
+	        }
+	    });
+	});
+})
