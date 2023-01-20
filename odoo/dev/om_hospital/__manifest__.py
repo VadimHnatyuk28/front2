99a2f29e0400f5403c9f167f07a189a390a393f1
VadimHnatyuk28 <vadim.gnatyuk28@gmail.com>
2023-01-20 12:08:49 +0200

    odoo

---
+++ odoo/dev/om_hospital/__manifest__.py
@@ -0,0 +1,9 @@
+{
+     'name': 'Hospital Management System',
+     'author': 'VadimHnatiuk',
+     'application': True,
+     'data': [
+          'views/menu.xml',
+          'views/patient.xml'
+     ]
+}
