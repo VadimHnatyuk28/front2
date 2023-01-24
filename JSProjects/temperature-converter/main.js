8895d02c2be6d2a88584771e022c7646bb1ce0d2
VadimHnatyuk28 <vadim.gnatyuk28@gmail.com>
2023-01-24 17:46:32 +0200

    add adaptive for phone and laptop

--- JSProjects/temperature-converter/main.js
+++ JSProjects/temperature-converter/main.js
@@ -23,16 +23,16 @@ for (let i = 0; i < inputs.length; i++) {
         
         switch (e.target.name) {
             case "celcius":
-                fahrenheitInput.value = (value * 1.8) + 32;
-                kelvinInput.value = value + 273.15;
+                fahrenheitInput.value = ((value * 1.8) + 32).toFixed(2);
+                kelvinInput.value = (value + 273.15).toFixed(2);
                 break;
             case "fahrenheit":
-                celciusInput.value = (value - 32) / 1.8;
-                kelvinInput.value = ((value - 32) / 1.8) + 273.15;
+                celciusInput.value = ((value - 32) / 1.8).toFixed(2);
+                kelvinInput.value = (((value - 32) / 1.8) + 273.15).toFixed(2);
                 break;
             case "kelvin":
-                celciusInput.value = value - 273.15;
-                fahrenheitInput.value = ((value - 273.15) * 1.8) + 32;
+                celciusInput.value = (value - 273.15).toFixed(2);
+                fahrenheitInput.value = (((value - 273.15) * 1.8) + 32).toFixed(2);
                 break;
         }
     });
